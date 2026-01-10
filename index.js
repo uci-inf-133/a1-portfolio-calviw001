1cea1e4958229ca4bc475169b459a38ca5f94997
calviw001 <cowvynyia@gmail.com>
2026-01-09 23:15:05 -0800

    Updated dark mode function
    The toggleDarkMode() function will now add or remove the 'dark-mode' depending on whether 'dark-mode' is stored as either 'off' or 'on'. My next step is for the dark mode to be loaded after I change html pages.

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-let darkModeButton = document.getElementById("darkModeButton");
+let darkModeButton = document.getElementById('darkModeButton');
 
 const toggleDarkMode = () => {
     let body = document.querySelector('body');
-    body.classList.toggle("dark-mode");
-}
+    let darkmode = localStorage.getItem('dark-mode');
+
+    if (darkmode === 'on') {
+        body.classList.remove('dark-mode')
+        localStorage.setItem('dark-mode', 'off')
+    }
+    else {
+        body.classList.add('dark-mode')
+        localStorage.setItem('dark-mode', 'on')
+    }
+};
 
 darkModeButton.addEventListener('click', toggleDarkMode);

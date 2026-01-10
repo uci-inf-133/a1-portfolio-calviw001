6f48f3775cfb1d7dec578b95173b31f81cda7526
calviw001 <cowvynyia@gmail.com>
2026-01-09 20:46:51 -0800

    Removed debugging comments

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 let darkModeButton = document.getElementById("darkModeButton");
 
 const toggleDarkMode = () => {
-    // Debug 1: Is button click registering?
-    console.log("✅ Button clicked!");
     let body = document.querySelector('body');
     body.classList.toggle("dark-mode");
-    // Debug: Check if class is being added
-    console.log("Body classes:", body.classList);
-    console.log("Has dark-mode?", body.classList.contains("dark-mode"));
 }
 
 darkModeButton.addEventListener('click', toggleDarkMode);
